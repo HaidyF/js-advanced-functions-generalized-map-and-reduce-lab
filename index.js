297800643aa7ff2fe62bb764ead7aeb400c93f2b
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-03-27 22:56:53 -0400

    commit tree a4fc5d4fc7ce02fd4f126af443d1b3a2c47b5dea

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(obj, c) {
+    const array = []
+    for (const key in obj) {
+        array.push(c(obj[key]))
+    }
+    return array
+}
+
+function reduce(obj, c, accumulator) {
+    const keys = Object.keys(obj)
+    let i = 1
+  
+    if (accumulator) {
+      i = 0
+    } else {
+      accumulator = obj[keys[0]]
+    }
+  
+    for (i; i < keys.length; i++) {
+      accumulator = c(accumulator, obj[keys[i]])
+    }
+    return accumulator
+  }
